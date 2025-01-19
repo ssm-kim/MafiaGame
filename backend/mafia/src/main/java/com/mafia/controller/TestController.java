2597175ec84c66e2b8e31a3d23b517c6a24e5b02
김성민 <rlacjs546@naver.com>
2025-01-19 17:22:31 +0900

    controller test

---
+++ backend/mafia/src/main/java/com/mafia/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.mafia.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Mafia Test !!";
+    }
+}
