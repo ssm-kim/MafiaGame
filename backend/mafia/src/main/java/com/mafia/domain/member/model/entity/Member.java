98fa72771eda98f7d373200be0a3776d17bc2c2d
배준영 <bjy556@naver.com>
2025-01-24 10:07:17 +0900

    feat: 회원 관리 기능 구현 (조회/수정/비활성화)

---
+++ backend/mafia/src/main/java/com/mafia/domain/member/model/entity/Member.java
@@ -0,0 +1,44 @@
+package com.mafia.domain.member.model.entity;
+
+import com.mafia.global.common.model.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(unique = true)
+    private String providerId;
+    private String email;
+    private String nickname;
+
+    public Member(Long memberId) {
+        this.memberId = memberId;
+    }
+    public static Member of(String providerId, String nickname, String email) {
+        return Member.builder()
+                .providerId(providerId)
+                .nickname(nickname)
+                .email(email)
+                .build();
+    }
+
+    public void changeNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
