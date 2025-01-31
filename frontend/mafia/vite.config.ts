72501e8121688a906b5ab9632c556818d349ba3b
전준표 <junpyo0920@gmail.com>
2025-01-31 12:50:19 +0900

    Chore: vite config 변경
    - import alias 추가
    - 모든 IP 주소(LAN과 공용 주소 포함)에서 서버 접근을 허용

--- frontend/mafia/vite.config.ts
+++ frontend/mafia/vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   define: {
     global: {},
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    host: true,
+  }
 })
