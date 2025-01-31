6966affecc0f88714bc01d09d2dcea6dae78deeb
배준영 <bjy556@naver.com>
2025-01-31 13:06:01 +0900

    feat: 로그아웃 구현

--- backend/mafia/src/main/java/com/mafia/domain/login/controller/LoginController.java
+++ backend/mafia/src/main/java/com/mafia/domain/login/controller/LoginController.java
@@ -1,11 +1,18 @@
 package com.mafia.domain.login.controller;
 
 
+import com.mafia.domain.login.model.dto.CustomOAuth2User;
+import com.mafia.domain.login.utils.CookieUtil;
 import com.mafia.global.common.model.dto.BaseResponse;
+import com.mafia.global.common.service.RedisService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +20,34 @@ import static com.mafia.global.common.model.dto.BaseResponseStatus.AUTHORIZATION
 
 @Slf4j
 @RestController
-@RequestMapping("/api/login")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class LoginController {
- 
-    @GetMapping("/success")
+
+    private final RedisService redisService;
+    private final CookieUtil cookieUtil;
+
+    @GetMapping("/login/success")
     public ResponseEntity<BaseResponse<Void>> loginSuccess() {
         return ResponseEntity.ok(new BaseResponse<>(AUTHORIZATION_SUCCESS));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<BaseResponse<Void>> logout(@AuthenticationPrincipal CustomOAuth2User detail,
+    HttpServletResponse response) {
+        // Redis에서 Refresh 토큰 제거
+        String providerId = detail.getProviderId();
+        redisService.delete(providerId);
+
+        // 쿠키 제거
+        Cookie accessCookie = cookieUtil.createCookie("ACCESS", null);
+        Cookie refreshCookie = cookieUtil.createCookie("REFRESH", null);
+        accessCookie.setMaxAge(0);  // 즉시 만료
+        refreshCookie.setMaxAge(0); // 즉시 만료
+
+        response.addCookie(accessCookie);
+        response.addCookie(refreshCookie);
+
+        return ResponseEntity.ok(new BaseResponse<>());
     }
 }
