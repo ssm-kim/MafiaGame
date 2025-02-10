22cc651df1b3261dc8589481cd51080285e4f5aa
배준영 <bjy556@naver.com>
2025-02-10 18:04:36 +0900

    feat : mattermost 알림 연동

--- backend/mafia/src/main/java/com/mafia/global/common/exception/handler/GlobalExceptionHandler.java
+++ backend/mafia/src/main/java/com/mafia/global/common/exception/handler/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.mafia.global.common.exception.handler;
 
+import com.mafia.domain.mattermost.NotificationManager;
 import com.mafia.global.common.exception.exception.BusinessException;
 import com.mafia.global.common.model.dto.BaseResponse;
 import com.mafia.global.common.model.dto.BaseResponseStatus;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +22,31 @@ public class GlobalExceptionHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
+    @Autowired
+    private NotificationManager notificationManager;
+
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<BaseResponse<BaseResponseStatus>> handleBusinessException(
         BusinessException e) {
         LOG.error("BusinessException occurred: {}", e.getMessage());
         return ResponseEntity.status(e.getBaseResponseStatus().getHttpStatus())
             .body(new BaseResponse<>(e.getBaseResponseStatus()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e, HttpServletRequest req) {
+        LOG.error("Exception occurred: {}", e.getMessage());
+        notificationManager.sendNotification(e, req.getRequestURI(), getParams(req));
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private String getParams(HttpServletRequest req) {
+        StringBuilder params = new StringBuilder();
+        Enumeration<String> keys = req.getParameterNames();
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            params.append("- ").append(key).append(": ").append(req.getParameter(key)).append("\n");
+        }
+        return params.toString();
     }
 }
