0f68d13176817d41b41f1bccfd80877369eb8dac
전준표 <junpyo0920@gmail.com>
2025-02-18 07:01:02 +0900

    refactor: WebSocket 커스텀 훅 적용

--- frontend/mafia/src/pages/game/GameRoom.tsx
+++ frontend/mafia/src/pages/game/GameRoom.tsx
@@ -1,18 +1,16 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { CompatClient } from '@stomp/stompjs';
 import roomApi from '@/api/roomApi';
 import { Room, ParticipantMap } from '@/types/room';
 import { ChatMessage } from '@/types/chat';
 import GameHeader from '@/components/gameroom/GameHeader';
-// import GameStatus from '@/components/gameroom/GameStatus';
 import ChatWindow from '@/components/gameroom/ChatWindow';
 import WaitingRoom from '@/components/gameroom/WaitingRoom';
 import { Player } from '@/types/player';
-<<<<<<< HEAD
 import GameComponent from '@/game/GameComponent';
-=======
->>>>>>> 6328bf018a318250cc83eef96f419a3645ef30c0
+import useWebSocket from '@/hooks/useWebSocket';
 
 export interface Participant {
   memberId: number;
@@ -28,7 +26,7 @@ function GameRoom(): JSX.Element {
   const { roomId } = useParams();
   const navigate = useNavigate();
   const [messages, setMessages] = useState<ChatMessage[]>([]);
-  const [newMessage, setNewMessage] = useState('');
+  const [userMessage, setUserMessage] = useState('');
   const [gameState, setGameState] = useState<Room | null>(null);
   const [participantNo, setParticipantNo] = useState<number | null>(null);
   const [participants, setParticipants] = useState<ParticipantMap | null>(null);
@@ -36,17 +34,19 @@ function GameRoom(): JSX.Element {
   const [isHost, setIsHost] = useState(false);
   const [currentNickname, setCurrentNickname] = useState<string>('');
   const [requiredPlayers, setRequiredPlayers] = useState<number>(8);
-  const [currentChatType, setCurrentChatType] = useState<'ROOM' | 'DAY' | 'NIGHT' | 'DEAD'>('ROOM');
-  const stompClientRef = useRef<any>(null);
+
+  const eventEmitter = useRef<Phaser.Events.EventEmitter>(new Phaser.Events.EventEmitter());
+
+  const webSocket = useWebSocket(roomId);
 
   useEffect(() => {
+    // 닉네임 가져오기
     const fetchNickname = async () => {
       try {
         const response = await axios.get('/api/member');
         if (response.data.isSuccess) {
           const { nickname } = response.data.result;
           setCurrentNickname(nickname);
-          console.log('API에서 가져온 nickname:', nickname);
         }
       } catch (error) {
         console.error('Failed to fetch nickname:', error);
@@ -55,31 +55,51 @@ function GameRoom(): JSX.Element {
     fetchNickname();
   }, []);
 
-  const handleMessage = (type: string, message: string) => {
-    console.log('Message type:', type);
-    console.log('Raw message:', message);
-    const parsedMessage = JSON.parse(message);
-    console.log('Parsed message:', parsedMessage);
+  useEffect(() => {
+    if (!roomId) return;
 
-    setMessages((prev) => [
-      ...prev,
-      {
-        id: parsedMessage.messageId || Date.now().toString(),
-        content: parsedMessage.content,
-        senderName: type === 'SYSTEM' ? 'SYSTEM' : parsedMessage.nickname || parsedMessage.sender,
-        timestamp: parsedMessage.sendTime || parsedMessage.timestamp || new Date().toISOString(),
-        type,
-      } as ChatMessage,
-    ]);
-  };
+    // 방 정보 가져오기
+    const fetchRoomInfo = async () => {
+      try {
+        const response = await roomApi.getRoom(Number(roomId));
+
+        if (response.data.isSuccess) {
+          const roomInfo = response.data.result;
+          setGameState(roomInfo);
+          setRequiredPlayers(roomInfo.requiredPlayers);
+        }
+      } catch (error) {
+        console.error('Failed to fetch room information:', error);
+        navigate('/game-lobby');
+      }
+    };
+
+    // 이전 채팅 내역 가져오기
+    const fetchChatHistory = async () => {
+      try {
+        const response = await axios.get(`/api/chat`, {
+          params: {
+            gameId: roomId,
+            chatType: 'room', // 소문자로 변경
+            count: 50,
+          },
+        });
+
+        if (response.data.isSuccess) {
+          const chatHistories = chatResponse.data.result;
+          setMessages(chatHistories);
+        }
+      } catch (error) {
+        console.error('Failed to fetch chat histories:', error);
+      }
+    };
+
+    fetchRoomInfo();
+    fetchChatHistory();
+  }, [roomId]);
 
   useEffect(() => {
     if (!participants) return;
-
-    console.log('----------------');
-    console.log('participants 업데이트:', participants);
-    console.log('currentNickname:', currentNickname);
-    console.log('현재 isHost 상태:', isHost);
 
     const playersList: Player[] = [];
 
@@ -108,270 +128,54 @@ function GameRoom(): JSX.Element {
       }
     });
 
-    console.log('생성된 playersList:', playersList);
     setPlayers(playersList);
   }, [participants, currentNickname]);
 
+  const handleMessage = (newMessage: ChatMessage) => {
+    setMessages((prev) => [...prev, newMessage]);
+  };
+
   useEffect(() => {
-    if (!stompClientRef.current) return;
+    if (!webSocket.stompClient?.connected || !currentNickname) return;
 
+    // 방 구독
+    webSocket.room.subscribeRoom(currentNickname, (message, gameStateChanged) => {
+      if (gameStateChanged) {
+        setGameState((prevState) => {
+          if (!prevState) return null;
+          return {
+            ...prevState,
+            roomStatus: 'PLAYING',
+          };
+        });
+
+        return;
+      }
+
+      setParticipants(message);
+    });
+
+    // 방 입장 메시지 송신
+    webSocket.room.enterRoom();
+
+    // 시스템 채팅 구독
+    webSocket.chatting.subscribeSystemChat((message) => {
+      eventEmitter.current?.emit('TIME', message);
+    });
+
+    // 방 채팅 구독
+    webSocket.chatting.subscribeRoomChat(handleMessage);
+  }, [webSocket.isConnected, currentNickname]);
+
+  useEffect(() => {
     if (gameState?.roomStatus === 'PLAYING' && gameState.participant[currentNickname]) {
-      const playerSubscriptions = gameState.participant[currentNickname].subscriptions || [];
+      const subscriptions = gameState.participant[currentNickname]?.subscriptions || [];
 
-      playerSubscriptions.forEach((subscription) => {
-        if (subscription.includes('day')) {
-          stompClientRef.current.subscribe(
-            `/topic/game-${roomId}-day-chat`,
-            (msg: { body: string }) => handleMessage('DAY', msg.body),
-          );
-          setCurrentChatType('DAY');
-        }
-        if (subscription.includes('night')) {
-          stompClientRef.current.subscribe(
-            `/topic/game-${roomId}-night-chat`,
-            (msg: { body: string }) => handleMessage('NIGHT', msg.body),
-          );
-          setCurrentChatType('NIGHT');
-        }
-        if (subscription.includes('dead')) {
-          stompClientRef.current.subscribe(
-            `/topic/game-${roomId}-dead-chat`,
-            (msg: { body: string }) => handleMessage('DEAD', msg.body),
-          );
-          setCurrentChatType('DEAD');
-        }
-        if (subscription.includes('system')) {
-          stompClientRef.current.subscribe(
-            `/topic/game-${roomId}-system`,
-            (msg: { body: string }) => handleMessage('SYSTEM', msg.body),
-          );
-        }
+      webSocket.chatting.subscribeTopics(subscriptions, true, (newMessage) => {
+        handleMessage(newMessage);
       });
     }
-  }, [roomId, gameState?.roomStatus, currentNickname]);
-
-  useEffect(() => {
-    console.log('실험----------------');
-    console.log('GameRoom - isHost 상태:', isHost);
-    console.log('GameRoom - players:', players);
-  }, [isHost, players]);
-
-  useEffect(() => {
-    let roomSubscription: any = null;
-    let chatSubscription: any = null;
-
-    const initializeRoom = async () => {
-      try {
-        if (!roomId) return;
-
-        await roomApi.initializeWebSocket();
-        const stompClient = roomApi.getStompClient();
-        stompClientRef.current = stompClient;
-
-        const responseNickname = await axios.get('/api/member');
-        const { nickname } = responseNickname.data.result;
-
-        if (stompClient) {
-          roomSubscription = roomApi.subscribeRoom(Number(roomId), (message) => {
-            if ('gameStart' in message && message.gameStart === 'true') {
-              setGameState((prevState) => {
-                if (!prevState) return null;
-                return {
-                  ...prevState,
-                  roomStatus: 'PLAYING',
-                };
-              });
-              return;
-            }
-
-            let isHostLeft = true;
-            Object.values(message as ParticipantMap).forEach((participantInfo) => {
-              if (participantInfo.participantNo === 1) {
-                isHostLeft = false;
-              }
-            });
-
-            if (isHostLeft) return navigate('/game-lobby');
-
-            const myNewInfo = Object.values(message).find((p) => p.nickname === nickname);
-
-            if (!myNewInfo) {
-              alert('강제 퇴장 당하였습니다.');
-              navigate('/game-lobby');
-            }
-
-            if (!message) {
-              alert('방이 삭제되었습니다.');
-              navigate('/game-lobby');
-              return;
-            }
-            setParticipants(message as ParticipantMap);
-          });
-
-          chatSubscription = stompClient.subscribe(
-            `/topic/room-${roomId}-chat`,
-            (msg: { body: string }) => handleMessage('ROOM', msg.body),
-          );
-
-          await roomApi.joinRoom(Number(roomId));
-        }
-
-        const response = await roomApi.getRoom(Number(roomId));
-        if (response.data.isSuccess) {
-          const room = response.data.result;
-          if (room) {
-            setGameState(room);
-            setRequiredPlayers(room.requiredPlayers);
-            try {
-              console.log('채팅 내역 요청 시작');
-              const chatResponse = await axios.get(`/api/chat`, {
-                params: {
-                  gameId: roomId,
-                  chatType: 'room', // 소문자로 변경
-                  count: 50,
-                },
-              });
-              console.log('채팅 API 응답:', chatResponse);
-
-              if (chatResponse?.data?.isSuccess) {
-                console.log('채팅 데이터:', chatResponse.data.result);
-                if (Array.isArray(chatResponse.data.result)) {
-                  setMessages(chatResponse.data.result);
-                } else {
-                  console.log('채팅 데이터가 배열이 아님:', chatResponse.data.result);
-                }
-              } else {
-                console.log('채팅 API 실패:', chatResponse?.data);
-              }
-            } catch (error) {
-              console.log('채팅 요청 에러:', error);
-            }
-          } else {
-            navigate('/game-lobby');
-          }
-        }
-      } catch (error) {
-        console.error('Failed to initialize room:', error);
-        navigate('/game-lobby');
-      }
-    };
-
-    initializeRoom();
-
-    return () => {
-      if (roomSubscription) roomSubscription.unsubscribe();
-      if (chatSubscription) chatSubscription.unsubscribe();
-      window.sessionStorage.removeItem(`room-${roomId}-entered`);
-      roomApi.disconnect();
-    };
-  }, [roomId, navigate]);
-<<<<<<< HEAD
-=======
-  // useEffect(() => {
-  //   let roomSubscription: any = null;
-  //   let chatSubscription: any = null;
-
-  //   const initializeRoom = async () => {
-  //     try {
-  //       if (!roomId) return;
-
-  //       await roomApi.initializeWebSocket();
-  //       const stompClient = roomApi.getStompClient();
-  //       stompClientRef.current = stompClient;
-
-  //       const responseNickname = await axios.get('/api/member');
-  //       const { nickname } = responseNickname.data.result;
-
-  //       if (stompClient) {
-  //         // roomSubscription = roomApi.subscribeRoom(Number(roomId), (message) => {
-  //         //   if ('gameStart' in message && message.gameStart === 'true') {
-  //         //     setGameState((prevState) => ({
-  //         //       // ...(gameState as Room),
-  //         //       // roomStatus: 'PLAYING',
-  //         //       ...prevState, // 기존의 모든 상태 유지
-  //         //       roomStatus: 'PLAYING',
-  //         //       participant: prevState?.participant || {},
-  //         //     }));
-
-  //         //     return;
-  //         roomSubscription = roomApi.subscribeRoom(Number(roomId), (message) => {
-  //           if ('gameStart' in message && message.gameStart === 'true') {
-  //             setGameState((prevState) => {
-  //               if (!prevState) return null; // prevState가 null이면 null 반환
-
-  //               return {
-  //                 ...prevState, // 기존 상태 모두 유지
-  //                 roomStatus: 'PLAYING', // roomStatus만 변경
-  //               };
-  //             });
-  //             return;
-  //           }
-
-  //           let isHostLeft = true;
-  //           // Object.values(message).forEach((participantInfo) => {
-  //           Object.values(message as ParticipantMap).forEach((participantInfo) => {
-  //             if (participantInfo.participantNo === 1) {
-  //               isHostLeft = false;
-  //             }
-  //           });
-
-  //           if (isHostLeft) return navigate('/game-lobby');
-
-  //           const myNewInfo = Object.values(message).find((p) => p.nickname === nickname);
-
-  //           if (!myNewInfo) {
-  //             alert('강제 퇴장 당하였습니다.');
-  //             navigate('/game-lobby');
-  //           }
-
-  //           if (!message) {
-  //             alert('방이 삭제되었습니다.');
-  //             navigate('/game-lobby');
-  //             return;
-  //           }
-  //           setParticipants(message as ParticipantMap);
-  //         });
-
-  //         chatSubscription = stompClient.subscribe(
-  //           `/topic/room-${roomId}-chat`,
-  //           (msg: { body: string }) => handleMessage('ROOM', msg.body),
-  //         );
-
-  //         await roomApi.joinRoom(Number(roomId));
-  //       }
-
-  //       const response = await roomApi.getRoom(Number(roomId));
-  //       if (response.data.isSuccess) {
-  //         const room = response.data.result;
-  //         if (room) {
-  //           setGameState(room);
-  //           setRequiredPlayers(room.requiredPlayers);
-  //           const chatResponse = await axios.get(`/chat?gameId=${roomId}&chatType=ROOM&count=50`);
-  //           console.log('채팅 응답:', chatResponse);
-  //           if (chatResponse.data.isSuccess) {
-  //             setMessages(chatResponse.data.result);
-  //           }
-  //         } else {
-  //           console.error('Failed to fetch chat history:', chatResponse.data);
-  //           navigate('/game-lobby');
-  //         }
-  //       }
-  //     } catch (error) {
-  //       console.error('Failed to initialize room:', error);
-  //       navigate('/game-lobby');
-  //     }
-  //   };
-
-  //   initializeRoom();
-
-  //   return () => {
-  //     if (roomSubscription) roomSubscription.unsubscribe();
-  //     if (chatSubscription) chatSubscription.unsubscribe();
-  //     window.sessionStorage.removeItem(`room-${roomId}-entered`);
-  //     roomApi.disconnect();
-  //   };
-  // }, [roomId, navigate]);
->>>>>>> 6328bf018a318250cc83eef96f419a3645ef30c0
+  });
 
   const handleLeaveRoom = async () => {
     try {
@@ -418,20 +222,6 @@ function GameRoom(): JSX.Element {
       }
 
       await roomApi.startGame(Number(roomId));
-<<<<<<< HEAD
-=======
-
-      // const response = await roomApi.startGame(Number(roomId));
-      // if (response.data.isSuccess) {
-      //   const gameStartData = response.data.result as GameStartResponse;
-      //   setGameState((prevState) => ({
-      //     ...prevState!,
-      //     roomStatus: gameStartData.roomStatus ? 'PLAYING' : 'WAITING',
-      //     participant: gameStartData.participant,
-      //   }));
-      // }
-      // 지우기 가능
->>>>>>> 6328bf018a318250cc83eef96f419a3645ef30c0
     } catch (error) {
       console.error('Failed to start game:', error);
       if (error instanceof Error) {
@@ -444,19 +234,11 @@ function GameRoom(): JSX.Element {
 
   const handleSendMessage = (e: React.FormEvent) => {
     e.preventDefault();
-    if (!newMessage.trim() || !roomId || !stompClientRef.current) return;
+    if (!userMessage.trim() || !roomId || !webSocket.stompClient?.connected) return;
 
-    stompClientRef.current.send(
-      '/app/chat/send',
-      {},
-      JSON.stringify({
-        gameId: roomId,
-        content: newMessage,
-        chatType: currentChatType,
-      }),
-    );
+    webSocket.chatting.sendMessage(userMessage);
 
-    setNewMessage('');
+    setUserMessage('');
   };
 
   return (
@@ -475,7 +257,17 @@ function GameRoom(): JSX.Element {
 
         <div className="flex h-full gap-4 pt-16">
           <div className="flex-1">
-            {gameState?.roomStatus ? (
+            {gameState?.roomStatus === 'PLAYING' || gameState?.active ? (
+              <div className="w-full h-full bg-gray-900 bg-opacity-80 rounded-lg border border-gray-800">
+                <GameComponent
+                  roomId={roomId}
+                  playerId={participantNo || 1}
+                  stompClient={webSocket.stompClient}
+                  eventEmitter={eventEmitter.current}
+                  subscribeTopics={webSocket.chatting.subscribeTopics}
+                />
+              </div>
+            ) : (
               <WaitingRoom
                 players={players}
                 isHost={isHost}
@@ -484,22 +276,14 @@ function GameRoom(): JSX.Element {
                 onStart={handleGameStart}
                 roomId={Number(roomId)}
               />
-            ) : (
-              <div className="w-full h-full bg-gray-900 bg-opacity-80 rounded-lg border border-gray-800">
-<<<<<<< HEAD
-                <GameComponent />
-=======
-                {/* <GameStatus gameState={gameState} /> */}
->>>>>>> 6328bf018a318250cc83eef96f419a3645ef30c0
-              </div>
             )}
           </div>
           <ChatWindow
             messages={messages}
-            newMessage={newMessage}
-            onMessageChange={setNewMessage}
+            newMessage={userMessage}
+            onMessageChange={setUserMessage}
             onSendMessage={handleSendMessage}
-            chatType={currentChatType}
+            chatType={webSocket.chatting.chatType}
             currentNickname={currentNickname}
           />
         </div>
