488b41ebcce481351c38d8fc29e29e896ac0ac31
YDaewon <qkqh9779@gmail.com>
2025-02-19 13:50:49 +0900

    fix: 게임 로직 수정#17 - 엔딩 씬 DTO 수정

--- backend/mafia/src/main/java/com/mafia/domain/game/model/dto/EndGameInfoDto.java
+++ backend/mafia/src/main/java/com/mafia/domain/game/model/dto/EndGameInfoDto.java
@@ -1,9 +1,7 @@
 package com.mafia.domain.game.model.dto;
 
 import com.mafia.domain.game.model.game.Game;
-import com.mafia.domain.game.model.game.GameStatus;
 import com.mafia.domain.game.model.game.Player;
-import com.mafia.domain.game.model.game.Role;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
@@ -18,17 +16,7 @@ public class EndGameInfoDto {
         List<Player> players = new ArrayList<>(game.getPlayers().values());
         endPlayers = new ArrayList<>();
         for(Player player : players){
-            boolean isWin;
-            if(game.getGameStatus() == GameStatus.ZOMBIE_WIN && player.getRole() == Role.ZOMBIE){
-                isWin = true;
-            } else if(game.getGameStatus() == GameStatus.MUTANT_WIN && player.getRole() == Role.MUTANT){
-                isWin = true;
-            } else {
-                isWin = game.getGameStatus() == GameStatus.CITIZEN_WIN &&
-                    player.getRole() != Role.MUTANT && player.getRole() != Role.ZOMBIE;
-            }
-
-            EndPlayer ep = new EndPlayer(game.getPlayerNoByMemberId(player.getMemberId()), player, isWin);
+            EndPlayer ep = new EndPlayer(game.getPlayerNoByMemberId(player.getMemberId()), player);
             endPlayers.add(ep);
         }
     }
